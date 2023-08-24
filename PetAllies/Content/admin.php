e2dcfd54f0c0a7a4c866b0a4f6378468da6d6be5
MarkHenrySL <cupidpumpkin0215@gmail.com>
2023-08-24 20:13:54 +0800

    1

--- PetAllies/Content/admin.php
+++ PetAllies/Content/admin.php
@@ -18,7 +18,7 @@ session_start();
 <head>
 	<title></title>
 	<link rel="stylesheet" href="../MyStyles/adminstyle.css" type="text/css"/>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <!--Latest/Update Kit Fontawesome-->
   <script src="https://kit.fontawesome.com/acd6544335.js" crossorigin="anonymous"></script>
 </head>
@@ -39,8 +39,8 @@ session_start();
 
 	    <div class="head">
 		    <div class="col-div-6">
-        <span style="font-size:30px;cursor:pointer; color: white;" class="nav"  >&#9776; Dashboard</span>
-        <span style="font-size:30px;cursor:pointer; color: white;" class="nav2"  >&#9776; Dashboard</span>
+        <span style="font-size:30px;cursor:pointer; color: white;" class="nav"  >Dashboard</span>
+        <span style="font-size:30px;cursor:pointer; color: white;" class="nav2"  >Dashboard</span>
       </div>
 	
 	    <div class="col-div-6">
@@ -141,35 +141,6 @@ session_start();
         <div class="clearfix"></div>
     </div>
 
-
-  <!--This is the Javascript for the sidebar panel click to open/close function-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script>
-
-    $(".nav").click(function(){
-      $("#mySidenav").css('width','70px');
-      $("#main").css('margin-left','70px');
-      $(".logo").css('visibility', 'hidden');
-      $(".logo span").css('visibility', 'visible');
-      $(".logo span").css('margin-left', '-10px');
-      $(".icon-a").css('visibility', 'hidden');
-      $(".icons").css('visibility', 'visible');
-      $(".icons").css('margin-left', '-8px');
-        $(".nav").css('display','none');
-        $(".nav2").css('display','block');
-    });
-
-  $(".nav2").click(function(){
-      $("#mySidenav").css('width','300px');
-      $("#main").css('margin-left','300px');
-      $(".logo").css('visibility', 'visible');
-      $(".icon-a").css('visibility', 'visible');
-      $(".icons").css('visibility', 'visible');
-      $(".nav").css('display','block');
-        $(".nav2").css('display','none');
-  });
-
-  </script>
 </body>
 
 </html>
--- PetAllies/Inventory/inventory.php
+++ PetAllies/Inventory/inventory.php
@@ -1,4 +1,5 @@
 <?php
+include '../Connection/connect.php';
 
 session_start();
 
